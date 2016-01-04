4262cdc70dc747c54a69dc6cb9b630dbd66178ba
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-04 17:19:06 -0500

    s

--- eggG.js
+++ eggG.js
@@ -4,7 +4,7 @@ console.log("Starting SUPER AWESOME LIBRARY...");
 var date = new Date();
 
 if(date.getSeconds() % 2 === 0){
-console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!");
+console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!... \n But that Dosn't mean that you are off the hook!");
 } else{
   window.onclick = function(){
     var audio = new Audio('http://soundbible.com/grab.php?id=1567&type=mp3');
