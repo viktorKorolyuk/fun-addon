1e951e582fda3de24280124ff262cdb4641c1224
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-04 18:03:53 -0500

    last commit for fun

--- eggG.js
+++ eggG.js
@@ -3,13 +3,13 @@ console.log("Starting SUPER AWESOME LIBRARY...");
 
 var date = new Date();
 
-if(date.getSeconds() % 2 === 0){
+if(date.getMinutes() % 2 === 0){
 console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!... \n But that Dosn't mean that you are off the hook!");
 } else{
 
   window.onclick = function(){
     console.log("WORRK");
-  document.querySelector("h1").innerHTML = "ZABOOBAFOO";
+  document.querySelector("h1").innerHTML = "ZABOOMAFOO";
   document.body.style.backgroundImage = "url('https://pmctvline2.files.wordpress.com/2014/11/zoboomafoo-lemur-dead.jpg?w=300&h=208&crop=1')";
   var audio = new Audio('http://rmira.downloader.info/download.php?id=7302c7662be87cbf9727650240db998f');
 audio.play();
