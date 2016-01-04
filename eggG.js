74823114584dbbffe8c424da813b1de9c1aa6c88
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-04 17:48:56 -0500

    zoo

--- eggG.js
+++ eggG.js
@@ -8,8 +8,7 @@ console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to c
 } else{
 
   window.onclick = function(){
-  document.body.innerHTML = "<center><h2 style='color:red'>HELLO MY NAME IS 9000volt<h1>Z</h1></h2></center>";
-
+  
 };
 
 }
