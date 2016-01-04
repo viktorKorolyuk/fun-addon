02d8fe505f2b64b74922acca64c0c976339f0cf4
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-04 17:09:50 -0500

    added music

--- eggG.js
+++ eggG.js
@@ -1,6 +1,15 @@
 (function(){
 console.log("Starting SUPER AWESOME LIBRARY...");
 
+var date = new Date();
 
+if(date.getSeconds() % 2 === 0){
+console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!");
+} else{
+  window.onclick = function(){
+    var audio = new Audio('http://soundbible.com/grab.php?id=1567&type=mp3');
+    audio.play();
+  }
+}
 
 })();
