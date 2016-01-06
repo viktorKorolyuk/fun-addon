38012c92b62610c03fdf0a442bc827c943116104
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-05 23:47:50 -0500

    test delete

--- eggG.js
+++ eggG.js
@@ -21,7 +21,7 @@ cusDiv.style.backgroundColor = "rgba(0, 0, 0, 0.79)";
 cusDiv.style.width = "100%";
 cusDiv.style.height = "100%";
 cusDiv.style.top = "0px";
-cusDiv.innerHTML = "<center><h1 style='margin:3em; font-size:3em; color:#a11097'>Welcome [USER]. You have been chosen to play for an easter egg hunt on this webpage. If you want to participate click continue. If not click cancel</h1><br><br><button>CANCEL</button><button>Continue!</button></center>";
+cusDiv.innerHTML = "<center><h1 style='margin:3em; font-size:3em; color:#a11097'>Welcome [USER]. You have been chosen to play for an easter egg hunt on this webpage. If you want to participate click continue. If not click cancel</h1><br><br><button onclick="var remove = document.removeChild(cusDiv)">CANCEL</button><button>Continue!</button></center>";
 document.body.appendChild(cusDiv);
 
 };
