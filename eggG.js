b06dcadf7dd73f89a249de0a05b38d29c2510247
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-05 21:52:23 -0500

    co

--- eggG.js
+++ eggG.js
@@ -21,7 +21,7 @@ cusDiv.style.backgroundColor = "rgba(0, 0, 0, 0.53)";
 cusDiv.style.width = "100%";
 cusDiv.style.height = "100%";
 cusDiv.style.top = "0px";
-cusDiv.innerHTML = "<center><h1 style='margin:8em;'>Hi there! My name is 9000 Volts, and welcome to my website \n :)</h1></center>";
+cusDiv.innerHTML = "<center><h1 style='margin:8em; color:#a11097'>Hi there! My name is 9000 Volts, and welcome to my website \n :)</h1></center>";
 document.body.appendChild(cusDiv);
 
 };
