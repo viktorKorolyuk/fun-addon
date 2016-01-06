d34ff0a3bf6980e8f1fe4ac36881afa17c4017cc
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-05 21:37:59 -0500

    ghghjc

--- eggG.js
+++ eggG.js
@@ -6,18 +6,26 @@ var date = new Date();
 if(date.getMinutes() % 2 === 0){
 console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!... \n But that Dosn't mean that you are off the hook!");
 }
-/** else{
+ else{
 
   window.onclick = function(){
-    console.log("WORRK");
-  document.querySelector("h1").innerHTML = "ZABOOMAFOO";
-  document.header.style.backgroundImage = "url('https://pmctvline2.files.wordpress.com/2014/11/zoboomafoo-lemur-dead.jpg?w=300&h=208&crop=1')";
+    console.log("Audio Plays now");
+      //  document.querySelector("h1").innerHTML = "ZABOOMAFOO";
   var audio = new Audio('http://rmira.downloader.info/download.php?id=7302c7662be87cbf9727650240db998f');
-audio.play();
+  audio.play();
+
+var cusDiv = document.createElement("div");
+cusDiv.style.zIndex = "10000";
+cusDiv.style.position = "absolute";
+cusDiv.style.backgroundColor = "rgba(0, 0, 0, 0.53)";
+cusDiv.style.width = "100%";
+cusDiv.style.height = "100%";
+cusDiv.style.top = "0px";
+
 };
 
 }
-*/
-if(date.getDay() == 2){}
+
+
 
 })();
