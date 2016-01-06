536e3d6b8a971ea6c29838c50ceb3b926c293ce6
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-06 14:13:40 -0500

    forgot

--- eggG.js
+++ eggG.js
@@ -2,6 +2,7 @@
 console.log("Starting SUPER AWESOME LIBRARY...");
 
 var date = new Date();
+var clicked = false;
 
 if(date.getMinutes() % 2 === 0){
 console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!... \n But that Dosn't mean that you are off the hook!");
