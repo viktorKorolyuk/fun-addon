7f4369efccecceb58daf45c272ec67b2fd7e3705
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-06 14:08:05 -0500

    added

--- eggG.js
+++ eggG.js
@@ -9,6 +9,7 @@ console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to c
  else{
 
   window.onclick = function(){
+    if(cliked = false){
     console.log("Audio Plays now");
       //  document.querySelector("h1").innerHTML = "ZABOOMAFOO";
   var audio = new Audio('http://rmira.downloader.info/download.php?id=7302c7662be87cbf9727650240db998f');
@@ -22,9 +23,11 @@ cusDiv.style.width = "100%";
 cusDiv.style.height = "100%";
 cusDiv.style.top = "0px";
 cusDiv.id = 'over'
-cusDiv.innerHTML = "<center><h1 style='margin:3em; font-size:3em; color:#a11097'>Welcome [USER]. You have been chosen to play for an easter egg hunt on this webpage. If you want to participate click continue. If not click cancel</h1><br><br><button onclick='document.removeChild(document.getElementById('over'))'>CANCEL</button><button>Continue!</button></center>";
+var x = document.getElementById('over');
+cusDiv.innerHTML = "<center><h1 style='margin:3em; font-size:3em; color:#a11097'>Welcome [USER]. You have been chosen to play for an easter egg hunt on this webpage. If you want to participate click continue. If not click cancel</h1><br><br><button onclick='document.removeChild(x)'>CANCEL</button><button>Continue!</button></center>";
 document.body.appendChild(cusDiv);
-
+clicked = true;
+}
 };
 
 }
