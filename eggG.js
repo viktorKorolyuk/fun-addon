7e64d8feec0e386a9387e00e04c84df99eae365a
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-05 23:25:04 -0500

    seconds?

--- eggG.js
+++ eggG.js
@@ -3,7 +3,7 @@ console.log("Starting SUPER AWESOME LIBRARY...");
 
 var date = new Date();
 
-if(date.getMinutes() % 2 === 0){
+if(date.getSeconds() % 2 === 0){
 console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to click and get annoying sounds!... \n But that Dosn't mean that you are off the hook!");
 }
  else{
