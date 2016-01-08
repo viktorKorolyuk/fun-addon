133632af278b908f634371f327485816de4929b3
ViktorKorolyuk <viktor.korolyuk@gmail.com>
2016-01-07 21:39:40 -0500

    commir

--- eggG.js
+++ eggG.js
@@ -9,8 +9,8 @@ console.log("EVEN STEVEN ODD TODD, this is your lucky day! \n You dont have to c
 }
  else{
 
-  window.onKeyPress = function(){
-    if(event.keyCode = 16) {
+  window.onkeydown = function(e){
+    if(e.keyCode = 16) {
     console.log("Audio Plays now");
       //  document.querySelector("h1").innerHTML = "ZABOOMAFOO";
   var audio = new Audio('http://rmira.downloader.info/download.php?id=7302c7662be87cbf9727650240db998f');
